c11d1d7ed95f0488dfcb33ebb8b784bec9301c90
Cristian Garcia <153207626+Chris20385@users.noreply.github.com>
2024-10-10 13:13:22 -0500

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,8 @@
+#Cristian Garcia
+#10/10/2024
+
+#This program computes MPG for car.
+
+m= (int(input("Enter number of miles driven: ")))
+g=(int(input("Enter number of gallons: ")))
+print (str(m) + " miles per " + str(g) + " gallon have a nice trip")
